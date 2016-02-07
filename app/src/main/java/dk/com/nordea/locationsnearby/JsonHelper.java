51f0ae292dfc546f5ac32fd12741b77116072f87
erdal gökten <goktenerdal@gmail.com>
2016-02-07 16:37:04 +0200

    Added some changes and unit tests

--- app/src/main/java/dk/com/nordea/locationsnearby/JsonHelper.java
+++ app/src/main/java/dk/com/nordea/locationsnearby/JsonHelper.java
@@ -1,7 +1,5 @@
 package dk.com.nordea.locationsnearby;
 
-import android.support.v4.util.Pair;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -14,26 +12,11 @@ public final class JsonHelper {
 
     private static final String TAG = JsonHelper.class.getName();
 
-    public static String GET(String url, Pair<String, String>... params) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        sb.append(url);
-        sb.append("?");
-        for(int i = 0; i < params.length; i++){
-            if (i != 0)
-                sb.append(Constants.URL_PARAM_DELIMITER);
-            sb.append(params[i].first.replace(Constants.URL_WHITE_SPACE, Constants.URL_WHITE_SPACE_REPLACEMENT));
-            sb.append(Constants.URL_KEY_VALUE_DELIMITER);
-            sb.append(params[i].second.replace(Constants.URL_WHITE_SPACE, Constants.URL_WHITE_SPACE_REPLACEMENT));
-        }
-        return sendAndReceive(sb.toString());
-    }
-
-    private static String sendAndReceive(String uri) throws Exception {
+    public static String GET(URL url) throws Exception {
         StringBuilder sb = new StringBuilder();
         InputStreamReader ir = null;
         BufferedReader reader = null;
         try {
-            URL url = new URL(uri);
             URLConnection conn = url.openConnection();
             conn.setDoOutput(true);
 
