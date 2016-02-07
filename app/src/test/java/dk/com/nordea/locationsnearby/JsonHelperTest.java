e6a992c2ec9b8654da72b0408dba0ab087e62c84
erdal gökten <goktenerdal@gmail.com>
2016-02-07 16:37:16 +0200

    Added some changes and unit tests

--- app/src/test/java/dk/com/nordea/locationsnearby/JsonHelperTest.java
+++ app/src/test/java/dk/com/nordea/locationsnearby/JsonHelperTest.java
@@ -5,6 +5,8 @@ import android.support.v4.util.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.URL;
+
 /**
  * Created by erdal on 1.2.2016.
  */
@@ -14,12 +16,14 @@ public class JsonHelperTest {
     public void MakeJsonRequest(){
 
         try{
-            String json = JsonHelper.GET(Constants.FOURSQUARE_BASE_URL
+            URL url = URLConstructor.fromParameters(Constants.FOURSQUARE_BASE_URL
                     , Pair.create("v", Constants.FOURSQUARE_VERSION)
                     , Pair.create("client_id", Constants.FOURSQUARE_CLIENT_ID)
                     , Pair.create("client_secret", Constants.FOURSQUARE_CLIENT_SECRET)
                     , Pair.create("ll", "40.7" + "," + "-74")
                     , Pair.create("query", "sushi"));
+
+            String json = JsonHelper.GET(url);
 
             Assert.assertFalse(json.isEmpty());
         } catch (Exception ex){
