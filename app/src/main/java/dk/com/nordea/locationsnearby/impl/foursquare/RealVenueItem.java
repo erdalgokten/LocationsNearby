93aecf0241a99c4cb0c6ba5f021cc8ccabd2eb01
erdal gökten <goktenerdal@gmail.com>
2016-02-07 16:37:54 +0200

    Added some changes and unit tests

---
+++ app/src/main/java/dk/com/nordea/locationsnearby/impl/foursquare/RealVenueItem.java
@@ -0,0 +1,12 @@
+package dk.com.nordea.locationsnearby.impl.foursquare;
+
+import dk.com.nordea.locationsnearby.VenueItem;
+
+/**
+ * Created by erdal on 30.1.2016.
+ */
+public class RealVenueItem extends VenueItem {
+    public RealVenueItem(String id, String name, String address, String distance) {
+        super(id, name, address, distance);
+    }
+}
